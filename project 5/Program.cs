26d0c396cb1831427cef86f9f44ce38fc5deed73
Ismayilzada Anar <anarpi@code.local>
2023-12-21 19:02:20 +0400

    first

---
+++ project 5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace project_5
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+        }
+    }
+}
